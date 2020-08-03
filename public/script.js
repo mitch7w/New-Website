6d794eb56a20a137f52786ee8281c1fca9613de9
Mitch <mitch7w@gmail.com>
2020-08-03 16:08:52 +0200

    FInal spacing and style implementation

--- public/script.js
+++ public/script.js
@@ -6,22 +6,18 @@ const lightIcon = document.getElementById("lightIcon");
 
 if(theme) {
   body.classList.add(theme) ;
-  themeText.innerHTML = theme;
 }
 else {
   body.classList.add("Light");
   localStorage.setItem("theme", "Light") ;
-  themeText.innerHTML = "Light";
 }
 
 themeButton.onclick = () => {
   if (theme === "Light") {
     localStorage.setItem("theme", "Dark");
-    themeText.innerHTML = "Dark";
     lightIcon.classList.toggle("lightPressed") ;
   } else {
     localStorage.setItem("theme", "Light");
-    themeText.innerHTML = "Light";
   }
   body.classList.toggle("Light");
   body.classList.toggle("Dark");
