acba2fa766f60dded7b9ed48a9b40752e4142d6a
Mitch <mitch7w@gmail.com>
2020-08-03 15:54:27 +0200

    Implement theme switching

--- public/script.js
+++ public/script.js
@@ -1,6 +1,28 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0] ;
-const navbarLinks = document.getElementsByClassName("navbar-links")[0] ;
+const themeButton = document.getElementById("themeButton") ;
+const body = document.body ;
+const theme = localStorage.getItem("theme") ; //caching
+const themeText = document.getElementById("theme-text");
+const lightIcon = document.getElementById("lightIcon");
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active") ;
-}) ;
+if(theme) {
+  body.classList.add(theme) ;
+  themeText.innerHTML = theme;
+}
+else {
+  body.classList.add("Light");
+  localStorage.setItem("theme", "Light") ;
+  themeText.innerHTML = "Light";
+}
+
+themeButton.onclick = () => {
+  if (theme === "Light") {
+    localStorage.setItem("theme", "Dark");
+    themeText.innerHTML = "Dark";
+    lightIcon.classList.toggle("lightPressed") ;
+  } else {
+    localStorage.setItem("theme", "Light");
+    themeText.innerHTML = "Light";
+  }
+  body.classList.toggle("Light");
+  body.classList.toggle("Dark");
+  } ;
